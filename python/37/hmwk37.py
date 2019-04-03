5fb372ffedd9373cfafc6183adc17f19ac794df3
yehudis pick <pickyehudis@gmail.com>
2019-04-03 11:03:42 -0400

    '37'

---
+++ python/37/hmwk37.py
@@ -0,0 +1,6 @@
+name = 'Yehudis Pick'
+address = '726 Ave P Brooklyn, Ny 11223'
+siblings =['Yisroel','Azriel','Dovid','Shevy','Shira']
+print(f' Name: {name}\n Address: {address}\n Siblings: {siblings}')
+print(name[::3])
+print(siblings[-2][1:-1:])
